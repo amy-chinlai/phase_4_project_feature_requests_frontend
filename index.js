eeaca0e66ffbf66b054c8c081cf51ac787c28718
amy-chinlai <amy@actionnetwork.org>
2022-01-13 10:37:05 -0500

    Rendering name

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
     .then (function(response) {
         return response.json()
     })
-    .then(function(json) {
-        for (const element of json.data) {
-            console.log(element)
-        }
+    .then(requests => {
+        requests.data.forEach(request => {
+            const requestMarkup = ` 
+                <div class="request">
+                    <h2>${request.attributes.name}</h2>
+                </div> `
+
+                document.querySelector("#requests-container").innerHTML += requestMarkup
+        })
     })
 });
 
+// function appendRequest(e) {
+//     console.log("appending")
+//     // var h1 = document.createElement('h1')
+//     // h1.classList.add('request')
+//     // h1.innerHTML = `${e.name}`
+//     // document.appendChild(h1)
+// }
