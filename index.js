5c2982678e4c6993f02c41e77012c106fcdd169f
amy-chinlai <amy@actionnetwork.org>
2022-01-13 10:15:32 -0500

    Fetching and logging json from localhost

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("this is working")
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("The DOM has loaded");
+    fetch("http://localhost:7000/api/v1/requests")
+    .then (function(response) {
+        return response.json()
+    })
+    .then(function(json) {
+        for (const element of json.data) {
+            console.log(element)
+        }
+    })
+});
